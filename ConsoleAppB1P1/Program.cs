d5fab529d4c8853d2d88342da7855e58e6370904
Wanderiel <dpavel82@yandex.ru>
2023-03-09 22:19:17 +0300

    Всё в сборе

---
+++ ConsoleAppB1P1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppB1P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            ushort age;
+            short speed;
+            byte level;
+            sbyte modifer;
+            int lengthSegment;
+            float hitPoints;
+            double manaPionts;
+            char armorGrade;
+            bool isDead;
+        }
+    }
+}
---
+++ ConsoleAppB1P2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace iJunior3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string zodiacSign;
+            string post;
+            int age;
+            int salary;
+
+            Console.Write("Здравствуйте! Как вас зовут: ");
+            name = Console.ReadLine();
+
+            Console.Write("Сколько вам полных лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Кто вы по знаку зодиака: ");
+            zodiacSign = Console.ReadLine();
+
+            Console.Write("Какая у вас должность: ");
+            post = Console.ReadLine();
+
+            Console.Write("Сколько вы получаете в рублях: ");
+            salary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nВас зовут {name}, ваш взраст в годах {age}, " +
+                $"по гороскопу вы {zodiacSign}, занимаете должность {post} " +
+                $"и получаете в рублях {salary}.");
+
+            Console.ReadKey();
+        }
+    }
+}
